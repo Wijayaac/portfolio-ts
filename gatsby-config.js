be02d6d4e1cae3578e5c7d02d919f69c3ba26967
Wijayaac <wijayaac25@gmail.com>
2021-08-19 08:36:02 +0800

    🎨 change to bootstrap css

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,30 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://wijayaac.netlify.app",
     title: "portfolio-ts",
+    siteDescription:
+      "Portfolio project made using React.JS as a core of the website, and using Gatsby as a Dinamic Site Generator",
   },
   plugins: [
-    "gatsby-plugin-sass",
     "gatsby-plugin-image",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
+    // {
+    //   resolve: "gatsby-plugin-google-analytics",
+    //   options: {
+    //     trackingId: "",
+    //   },
+    // },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
         icon: "src/images/icon.png",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-sass",
+      options: {
+        implementation: require("sass"),
       },
     },
     "gatsby-transformer-remark",
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import "../style/main.scss";
+import Navbar from "../components/Navbar";
+// markup
+const IndexPage = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  );
+};
+
+export default IndexPage;
